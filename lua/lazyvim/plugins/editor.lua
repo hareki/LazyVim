e19671a3eea7c0f2c8ac459431b7b5e20c2d56b9
Tu Nguyen <minhtu1392000@gmail.com>
2024-10-31 21:12:13 +0700

    capitalize which-key group

--- lua/lazyvim/plugins/editor.lua
+++ lua/lazyvim/plugins/editor.lua
@@ -180,30 +180,30 @@ return {
       spec = {
         {
           mode = { "n", "v" },
-          { "<leader><tab>", group = "tabs" },
-          { "<leader>c", group = "code" },
-          { "<leader>f", group = "file/find" },
-          { "<leader>g", group = "git" },
-          { "<leader>gh", group = "hunks" },
-          { "<leader>q", group = "quit/session" },
-          { "<leader>s", group = "search" },
-          { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
-          { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
-          { "[", group = "prev" },
-          { "]", group = "next" },
-          { "g", group = "goto" },
-          { "gs", group = "surround" },
-          { "z", group = "fold" },
+          { "<leader><tab>", group = "Tabs" },
+          { "<leader>c", group = "Code" },
+          { "<leader>f", group = "File/Find" },
+          { "<leader>g", group = "Git" },
+          { "<leader>gh", group = "Hunks" },
+          { "<leader>q", group = "Quit/session" },
+          { "<leader>s", group = "Search" },
+          { "<leader>u", group = "UI", icon = { icon = "󰙵 ", color = "cyan" } },
+          { "<leader>x", group = "Diagnostics/Quickfix", icon = { icon = "󱖫 ", color = "green" } },
+          { "[", group = "Prev" },
+          { "]", group = "Next" },
+          { "g", group = "Goto" },
+          { "gs", group = "Surround" },
+          { "z", group = "Fold" },
           {
             "<leader>b",
-            group = "buffer",
+            group = "Buffer",
             expand = function()
               return require("which-key.extras").expand.buf()
             end,
           },
           {
             "<leader>w",
-            group = "windows",
+            group = "Windows",
             proxy = "<c-w>",
             expand = function()
               return require("which-key.extras").expand.win()
